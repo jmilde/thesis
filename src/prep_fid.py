e66b4b62f79f0b7632d63db96e101dbeb3d3fd6b
jmilde <jan.milde@hotmail.de>
2020-07-30 17:38:47 +0200

    added fid

---
+++ src/prep_fid.py
@@ -0,0 +1,43 @@
+import numpy as np
+import src.fid as fid
+from imageio import imread
+import tensorflow as tf
+import os
+import glob
+from tqdm import tqdm
+
+def calc_and_save_reference(data_path, output_path, inception_path=None):
+
+    #os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+    ########
+    # PATHS
+    ########
+    # if you have downloaded and extracted
+    #   http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
+    # set this path to the directory where the extracted files are, otherwise
+    # just set it to None and the script will later download the files for you
+    print("check for inception model..", end="\n", flush=True)
+    inception_path = fid.check_or_download_inception(inception_path) # download inception if necessary
+    print("ok")
+
+    # loads all images into memory (this might require a lot of RAM!)
+    print("load images..", end=" " , flush=True)
+    image_list = [os.path.join(data_path,fn) for fn in tqdm(os.listdir(data_path))][:100]
+
+    print("%d images found and loaded" % len(image_list))
+
+    print("create inception graph..", end=" ", flush=True)
+    fid.create_inception_graph(inception_path)  # load the graph into the current TF graph
+    print("ok")
+
+    print("calculte FID stats..", end=" ", flush=True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        mu, sigma = fid.calculate_activation_statistics(image_list, sess, batch_size=100)
+        np.savez_compressed(output_path, mu=mu, sigma=sigma)
+    print("finished")
+
+if __name__=="__main__":
+    calc_and_save_reference(data_path=os.path.expanduser("~/data/imgs"),
+                            output_path=os.path.expanduser("~/data/results"),
+                            inception_path=os.path.expanduser("~/data/"))

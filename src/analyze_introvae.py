fix mssim, restructure for normalization experiments

src/analyze_introvae.py
@@ -26,42 +26,42 @@ def calculate_scores(model, data, writer, path_fid, path_inception, model_name,
         os.mkdir(path_fid_data)
     sample_nr = 0
     imgs= []
-    for _ in tqdm(range(math.ceil(fid_samples_nr//batch_size))):
+    for _ in tqdm(range(math.ceil(fid_samples_nr//batch_size+1))):
         inpt = next(data)
         output = model.train(*inpt)
-        imgs.extend(output["x_p"].numpy())
-        for img in output["x_p"]:
-            if sample_nr<=fid_samples_nr:
-                imsave(os.path.join(path_fid_data, f"{sample_nr}.png"), np.clip(img, 0, 255).astype("uint8"))
-                sample_nr += 1
-
-            else:
-                break
+        imgs_array = output["x_p"].numpy()
+        imgs.extend(imgs_array)
+        #for img in imgs_array:
+            #if sample_nr<=fid_samples_nr:
+                #imsave(os.path.join(path_fid_data,
+                                    #f"{sample_nr}.png"),
+                       #np.clip(img*255, a_min=0, a_max=255).astype("uint8"))
+                #sample_nr += 1
+                #
+            #else:
+                #break
 
     print("Calculate MS-SSIM Score")
-    imgs = np.array(imgs)[:-1] if len(imgs)%2 else np.array(imgs)
-    imgs = np.clip(imgs, 0, 255)
-    split_a = imgs[:len(imgs)//2]
-    split_b = imgs[len(imgs)//2:]
-
-
     with tf.device('/CPU:0'):
+        imgs = np.clip(np.array(imgs[:50000])*255, 0 , 255)
         x = []
-        for a,b in zip(np.array_split(split_a, 5), np.array_split(split_b, 5)):
+        for a,b in zip(np.array_split(imgs[:len(imgs)//2], 5),
+                       np.array_split(imgs[len(imgs)//2:], 5)):
             x.extend( tf.image.ssim_multiscale(a,
                                                b,
                                                255,
-                                                   filter_size=8))
+                                           filter_size=8))
         ms_ssim = tf.math.reduce_mean(x)
         print(f"MS_SSIM: {ms_ssim}")
+
         with writer.as_default():
             tf.summary.scalar("MS_SSIM_score" , ms_ssim , step=0)
             writer.flush()
 
         print("caluclate mean and var")
         calc_and_save_reference(path_fid_data,
-                                os.path.join(path_fid, f"{model_name}.npz"),
-                                inception_path=path_inception)
+                            os.path.join(path_fid, f"{model_name}.npz"),
+                            inception_path=path_inception)
 
         print("calculate FID Score")
         mu1= np.load(os.path.join(path_fid, f"{model_name}.npz"), allow_pickle=True)["mu"]

src/hyperparameter.py
@@ -87,8 +87,8 @@ params = {
         "batch_size": 64,
         "logs_per_epoch": 100,  # log ~100x per epoch
         "fid_samples_nr": 50000,
-        'normalizer_enc': tf.keras.layers.BatchNormalization, # tfa.layers.InstanceNormalization,
-        'normalizer_dec': tf.keras.layers.BatchNormalization, # tfa.layers.GroupNormalization
+        'normalizer_enc': None, #None "instance" "batch" "group"
+        'normalizer_dec': None, #None "instance" "batch" "group"
 
         ### loss weights
         "weight_rec": 0.5, # beta: og:0.5, 0.01 - 100, larger β improves reconstruction quality but may influence sample diversity

src/models/introvae.py
@@ -181,29 +181,35 @@ class Encoder(tf.keras.layers.Layer):
     def __init__(self,
                  channels,
                  latent_dim,
-                 normalizer=tf.keras.layers.BatchNormalization,
+                 normalizer=None,
                  name="Encoder",
                  dropout_rate=0,
                  **kwargs):
         super(Encoder, self).__init__(name=name, **kwargs)
 
-        self.layers = [tf.keras.layers.Conv2D(channels[0],
+        self.layers = []
+        self.layers.append(tf.keras.layers.Conv2D(channels[0],
                                          kernel_size=5,
                                          strides=1,
                                          padding="same",
-                                         use_bias=False),
-                       normalizer(),
-                       tf.keras.layers.LeakyReLU(0.2),
-                       tf.keras.layers.AveragePooling2D()]
+                                         use_bias=False),)
+        #if normalizer:
+        #    self.layers.append(normalizer())
+        self.layers.append( tf.keras.layers.BatchNormalization())
+        self.layers.append(tf.keras.layers.LeakyReLU(0.2))
+        self.layers.append(tf.keras.layers.AveragePooling2D())
 
         for i, channel in enumerate(channels[1:], 1):
             self.layers.append(ResBlock(channel,
                                         adjust_channels=channel!=channels[i-1],
+                                        normalizer=normalizer,
                                         dropout_rate=dropout_rate))
             self.layers.append(tf.keras.layers.AveragePooling2D())
 
         # additional res layer
-        self.layers.append(ResBlock(channels[-1], dropout_rate=dropout_rate))
+        self.layers.append(ResBlock(channels[-1],
+                                    normalizer=normalizer,
+                                    dropout_rate=dropout_rate))
 
         self.variational = VariationalEncoding(latent_dim)
 
@@ -229,7 +235,7 @@ class Decoder(tf.keras.layers.Layer):
                  color_onehot_dim,
                  color_cond_type="one_hot",
                  txt_cond_type="rnn",
-                 normalizer=tf.keras.layers.BatchNormalization,
+                 normalizer=None,
                  name="Decoder",
                  **kwargs):
         super(Decoder, self).__init__(name=name, **kwargs)
@@ -258,16 +264,19 @@ class Decoder(tf.keras.layers.Layer):
         self.dense_resize     = tf.keras.layers.Dense(xy_dim*xy_dim*channels[0],
                                                       name="dense_resize")
         self.layers = []
-        self.layers.append(ResBlock(channels[0]))
+        self.layers.append(ResBlock(channels[0],
+                           normalizer=normalizer))
         self.layers.append(tf.keras.layers.UpSampling2D(size=(2,2)))
 
         for i,channel in enumerate(channels[1:], 1):
             self.layers.append(ResBlock(channel,
-                                        adjust_channels= channel!=channels[i-1]))
+                                        adjust_channels= channel!=channels[i-1],
+                                        normalizer=normalizer))
             self.layers.append(tf.keras.layers.UpSampling2D(size=(2,2)))
 
         # additional res layer
-        self.layers.append(ResBlock(channels[-1]))
+        self.layers.append(ResBlock(channels[-1],
+                                    normalizer=normalizer))
 
         self.layers.append(tf.keras.layers.Conv2D(inpt_dim[-1], kernel_size=5,
                                                   strides=1, padding="same"))

src/train_introvae.py
@@ -45,8 +45,7 @@ def main():
     epochs = p['epochs']
     batch_size = p['batch_size']
     logs_per_epoch = p['logs_per_epoch']
-    normalizer_enc = p['normalizer_enc']
-    normalizer_dec = p['normalizer_dec']
+
     weight_rec = p['weight_rec']
     weight_kl = p['weight_kl']
     weight_neg = p['weight_neg']
@@ -64,11 +63,27 @@ def main():
     fid_samples_nr = p["fid_samples_nr"]
     color_cond_dim = len(np.load(path_cond, allow_pickle=True)["colors_old" if color_cond_type=="one_hot" else "colors"][1])
 
+    if not p["normalizer_enc"]:
+        norm = "_NOBN"
+        normalizer_enc = None
+        normalizer_dec = None
+    elif p["normalizer_enc"]== "instance":
+        norm = ""#"_INST"
+        normalizer_enc = tfa.layers.InstanceNormalization
+        normalizer_dec = tfa.layers.InstanceNormalization
+    elif p["normalizer_enc"]== "group":
+        norm = "_GRP"
+        normalizer_enc = tfa.layers.GroupNormalization
+        normalizer_dec = tfa.layers.GroupNormalization
+    elif p["normalizer_enc"]== "batch":
+        norm = "_BATCH"
+        normalizer_enc = tf.keras.layers.BatchNormalization
+        normalizer_dec = tf.keras.layers.BatchNormalization
 
     if p["vae_epochs"] and p["epochs"]:
-        modeltype = f"INTRO{p['epochs']}_pre{p['vae_epochs']}-m{m_plus}-b1{beta1}b2{beta2}-w_rec{weight_rec}-w_neg{weight_neg}"
+        modeltype = f"INTRO{norm}_{p['epochs']}_pre{p['vae_epochs']}-m{m_plus}-b1{beta1}b2{beta2}-w_rec{weight_rec}-w_neg{weight_neg}"
     elif p["epochs"]:
-        modeltype = f"INTRO_{p['epochs']}-m{m_plus}-lr{lr_enc}b1{beta1}b2{beta2}-w_rec{weight_rec}-w_neg{weight_neg}"
+        modeltype = f"INTRO{norm}_{p['epochs']}-m{m_plus}-lr{lr_enc}b1{beta1}b2{beta2}-w_rec{weight_rec}-w_neg{weight_neg}"
     else:
         modeltype = f"VAE{p['vae_epochs']}-b1{beta1}b2{beta2}"
     txt_info   = f"txt:({txt_cond_type}-dense{cond_dim_txts}-rnn{rnn_dim}-emb{emb_dim})"  if txt_cond_type else ""
@@ -108,8 +123,8 @@ def main():
                      txt_cond_type,
                      dropout_conditionals=dropout_conditionals,
                      dropout_encoder_resblock=dropout_encoder_resblock,
-                     normalizer_enc = tf.keras.layers.BatchNormalization,
-                     normalizer_dec = tf.keras.layers.BatchNormalization,
+                     normalizer_enc = normalizer_enc,
+                     normalizer_dec = normalizer_dec,
                      weight_rec=weight_rec,
                      weight_kl=weight_kl,
                      weight_neg = weight_neg,
@@ -138,11 +153,12 @@ def main():
         ckpt.restore(manager.latest_checkpoint)
         print("\nmodel restored\n")
 
+
     # for logging
     example_data=next(data)
 
     if vae_epochs:
-        manager = tf.train.CheckpointManager(ckpt, path_ckpt, max_to_keep=3, checkpoint_name=model_name + "_VAEpretrain")
+        manager = tf.train.CheckpointManager(ckpt, path_ckpt, max_to_keep=10, checkpoint_name=model_name + "_VAEpretrain")
         step=0
         for _ in trange(vae_epochs, desc="epochs", position=0):
             for _ in trange(ds_size//batch_size, desc="steps in epochs", position=1, leave=False):
@@ -179,7 +195,7 @@ def main():
 
 
     # manager for intravae training
-    manager = tf.train.CheckpointManager(ckpt, path_ckpt, max_to_keep=3, checkpoint_name=model_name)
+    manager = tf.train.CheckpointManager(ckpt, path_ckpt, max_to_keep=10, checkpoint_name=model_name)
 
 
 

src/util_tf.py
@@ -76,7 +76,7 @@ class VariationalEncoding(tf.keras.layers.Layer):
 class ResBlock(tf.keras.layers.Layer):
     def __init__(self, nr_filters,
                  adjust_channels=False,
-                 normalizer=tf.keras.layers.BatchNormalization,
+                 normalizer=None,
                  activation=tf.keras.layers.LeakyReLU(alpha=0.2),
                  dropout_rate=0):
         super(ResBlock, self).__init__(name='ResBlock')
@@ -84,22 +84,25 @@ class ResBlock(tf.keras.layers.Layer):
 
         if adjust_channels: self.adjust = tf.keras.layers.Conv2D(nr_filters, kernel_size=1, padding="same", use_bias=False)
         else: self.adjust = None
-
         self.activation = activation
         self.conv1 = tf.keras.layers.Conv2D(nr_filters, kernel_size=3, padding="same", use_bias=False)
-        self.normalize1 = normalizer()
+
+        self.normalize1 = normalizer() if normalizer else None
+        self.normalize2 = normalizer() if normalizer else None
         self.conv2 = tf.keras.layers.Conv2D(nr_filters, kernel_size=3, padding="same", use_bias=False)
-        self.normalize2 = normalizer()
         self.dropout= tf.keras.layers.Dropout(dropout_rate)
 
     def call(self, inpt, training=False):
         with tf.name_scope("ResBlock") as scope:
             if self.adjust: inpt = self.adjust(inpt)
             x = self.conv1(inpt)
-            x = self.normalize1(x, training=training)
+            if self.normalize1:
+                x = self.normalize1(x, training=training)
             x = self.activation(x)
             x = self.conv2(x)
-            x = self.normalize2(x+inpt, training=training)
+            x = x+inpt
+            if self.normalize2:
+                x = self.normalize2(x, training=training)
             x = self.dropout(self.activation(x), training=training)
         return x
 
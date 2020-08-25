0cb81bcb154eadfcc624d2528e3724a27800aef7
jmilde <jan.milde@hotmail.de>
2020-08-25 09:43:57 +0200

    Minor

--- src/prep_vggconditionals.py
+++ src/prep_vggconditionals.py
@@ -4,27 +4,47 @@ from skimage import io
 import numpy as np
 from sklearn.cluster import KMeans
 from tqdm import tqdm
-print("get embeddings")
-path_imgs = os.path.expanduser("~/data/imgs/")
-path_conditionals = os.path.expanduser("~/data/eudata_conditionals.npz")
-with tf.device('/CPU:0'):
-    model = tf.keras.applications.VGG16(input_shape=(128,128,3),
-                                        include_top=False)
 
-    embeddings = []
-    for img in tqdm(os.listdir(path_imgs)):
-        x = io.imread(os.path.join(path_imgs, img))/255
-        embeddings.append(model(np.array([x])).numpy().flatten())
+def add_resnet_condtionals(path_imgs, path_conditionals):
+    with tf.device('/CPU:0'):
+        model = tf.keras.applications.VGG16(input_shape=(128,128,3),
+                                            include_top=False)
 
-print("start clustering")
-kmeans = KMeans(n_clusters=10).fit(embeddings)
-print("load existing conditionals")
-x = np.load(path_conditionals, allow_pickle=True)
+        embeddings = []
+        for img in tqdm(os.listdir(path_imgs)):
+            x = io.imread(os.path.join(path_imgs, img))/255
+            embeddings.append(model(np.array([x])).numpy().flatten())
 
-print("save conditionals")
-np.savez_compressed(path_conditionals,
-                    colors=x["colors"],
-                    txts=x["txts"],
-                    txt_embs = x["txt_embs"],
-                    colors_old = x["colors_old"],
-                    res_cluster= kmeans.labels_)
+    print("start clustering")
+    kmeans = KMeans(n_clusters=10).fit(embeddings)
+    print("load existing conditionals")
+    x = np.load(path_conditionals, allow_pickle=True)
+
+    print("save conditionals")
+    np.savez_compressed(path_conditionals,
+                        colors=x["colors"],
+                        txts=x["txts"],
+                        txt_embs = x["txt_embs"],
+                        colors_old = x["colors_old"],
+                        res_cluster= kmeans.labels_)
+    print("done")
+
+def main():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+    print("lld dataset")
+    path_imgs = os.path.expanduser("~/data/lld_boosted/")
+    path_conditionals = os.path.expanduser("~/data/lldboosted_conditionals.npz")
+    add_resnet_condtionals(path_imgs, path_conditionals)
+
+    print("only logos")
+    path_imgs = os.path.expanduser("~/data/only_logo/") #only_logo lld_boosted
+    path_conditionals = os.path.expanduser("~/data/onlylogo_conditionals.npz") #lldboosted
+    add_resnet_condtionals(path_imgs, path_conditionals)
+
+if __name__=="__main__":
+    main()
